65fb571be87de776528cdd60024e8e923c4b31cd
Darshan <darshan.qseksolutions@gmail.com>
2022-06-15 15:12:26 +0530

    Delete unnecessary theme

--- wp-content/themes/twentytwenty-child/header.php
+++ wp-content/themes/twentytwenty-child/header.php
@@ -39,7 +39,7 @@
 			</div>
 			<nav class="navbar navbar-expand-md bg-inverse fixed-top scrolling-navbar">
 				<div class="container">
-					<a href="index.html" class="navbar-brand">
+					<a href="" class="navbar-brand">
 						<!-- <img src="img/logo.png" alt=""> -->
 						<?php 
 						// Site title or logo.
@@ -59,33 +59,6 @@
 					<div class="collapse navbar-collapse" id="navbarCollapse">
 
 						<ul class="navbar-nav mr-auto w-100 justify-content-end">
-							<!-- <li class="nav-item">
-								<a class="nav-link page-scroll" href="#home">Home</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link page-scroll" href="#services">About</a>
-							</li>  
-							<li class="nav-item">
-								<a class="nav-link page-scroll" href="#features">Services</a>
-							</li>                            
-							<li class="nav-item">
-								<a class="nav-link page-scroll" href="#showcase">Showcase</a>
-							</li>       
-							<li class="nav-item">
-								<a class="nav-link page-scroll" href="#pricing">Pricing</a>
-							</li>     
-							<li class="nav-item">
-								<a class="nav-link page-scroll" href="#team">Team</a>
-							</li> 
-							<li class="nav-item">
-								<a class="nav-link page-scroll" href="#blog">Blog</a>
-							</li>  
-							<li class="nav-item">
-								<a class="nav-link page-scroll" href="#contact">Contact</a>
-							</li> 
-							<li class="nav-item">
-								<a class="btn btn-singin" href="#">Download</a>
-							</li> -->
 							<?php
 							if ( has_nav_menu( 'primary' ) ) {
 
@@ -121,18 +94,26 @@
 				<div class="row space-100">
 					<div class="col-lg-6 col-md-12 col-xs-12">
 						<div class="contents">
-							<h2 class="head-title">Handcrafted Web Template <br>For Business and Startups</h2>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab <br>dolores ea fugiat nesciunt quisquam.</p>
-							<div class="header-button">
-								<a href="#" class="btn btn-border-filled">Get Started</a>
-								<a href="#contact" class="btn btn-border page-scroll">Contact Us</a>
-							</div>
+							<h2 class="head-title"><?=the_field('head_title',55);?></h2>
+							<p><?=the_field('head_paragraph',55);?></p>
+							<?php if( have_rows('button',55) ){ ?>
+								<div class="header-button">
+									<?php  while( have_rows('button',55) ) { the_row(); ?>
+									<a href="<?=the_sub_field('button_url',55)['url'];?>" class="<?=the_sub_field('button_class',55);?>"><?=the_sub_field('button_name',55);?></a>
+								<?php } ?>
+								</div>
+							<?php } ?>
 						</div>
 					</div>
 					<div class="col-lg-6 col-md-12 col-xs-12 p-0">
-						<div class="intro-img">
-							<img src="img/intro.png" alt="">
-						</div>            
+						<?php  
+						$image = get_field('intro_image');
+						if( !empty( $image ) ){
+							?>
+							<div class="intro-img">
+								<img src="<?=esc_url($image['url']);?>" alt="<?=esc_attr($image['alt']);?>">
+							</div>            
+						<?php } ?>
 					</div>
 				</div> 
 			</div>           
